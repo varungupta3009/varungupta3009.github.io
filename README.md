d6c4f1bfb27394760d28365d1f575f66dc7771b0
Varun Gupta <varungupta3009@gmail.com>
2019-05-27 21:12:00 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# varungupta3009.github.io
-My Personal Site
---
+++ precache-manifest.ae50215aab9eda80173b2235404f7573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f87ed47bbcec0a1fd1d6963c79b6541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df8596bf9662a0c54f7",
+    "url": "/static/css/main.5f21706c.chunk.css"
+  },
+  {
+    "revision": "114339fa847c4b29338a",
+    "url": "/static/js/2.ab704891.chunk.js"
+  },
+  {
+    "revision": "8df8596bf9662a0c54f7",
+    "url": "/static/js/main.8ffe8585.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
