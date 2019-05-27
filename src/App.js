71a1c270523ce218bd24002e6209efbb82c35e33
Varun Gupta <varungupta3009@gmail.com>
2019-05-27 21:10:31 +0530

    Switch to Source, complete site deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import $ from 'jquery';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1></h1>
       </header>
     </div>
   );
 }
+
+$(document).ready(function () {
+  $(window).on("load", function () {
+    $("h1").fadeOut(0, function () {
+      $(this).text("Hi")
+    })
+    $("h1").fadeToggle(1000, function () {
+      $(this).delay(3000).fadeToggle(1000, function () {
+        $(this).text("We're setting things up for you")
+        $(this).fadeToggle(1000).delay(3000).fadeToggle(1000, function () {
+          $(".App-header").addClass("cycle")
+          $(this).text("This may take longer than usual")
+          $(this).fadeToggle(1000)
+        })
+      })
+    })
+  })
+});
 
 export default App;
