b053a9bb00f638c54730c1aa879dc77978d04777
Varun Gupta <varungupta3009@gmail.com>
2019-05-28 18:48:11 +0530

    Fix placeholder not showing
    Now it works.

--- src/App.js
+++ src/App.js
@@ -14,18 +14,16 @@ function App() {
 }
 
 $(document).ready(function () {
-  $(window).on("load", function () {
-    $("h1").fadeOut(0, function () {
-      $(this).text("Hi")
-    })
-    $("h1").fadeToggle(1000, function () {
-      $(this).delay(3000).fadeToggle(1000, function () {
-        $(this).text("We're setting things up for you")
-        $(this).fadeToggle(1000).delay(3000).fadeToggle(1000, function () {
-          $(".App-header").addClass("cycle")
-          $(this).text("This may take longer than usual")
-          $(this).fadeToggle(1000)
-        })
+  $("h1").fadeOut(0, function () {
+    $(this).text("Hi")
+  })
+  $("h1").fadeToggle(1000, function () {
+    $(this).delay(3000).fadeToggle(1000, function () {
+      $(this).text("We're setting things up for you")
+      $(this).fadeToggle(1000).delay(3000).fadeToggle(1000, function () {
+        $(".App-header").addClass("cycle")
+        $(this).text("This may take longer than usual")
+        $(this).fadeToggle(1000)
       })
     })
   })
